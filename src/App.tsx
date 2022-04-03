b28bef040c1b8057c85cd8b6c20b9779b1cf46df
marieStamp <mariemarkina@gmail.com>
2022-04-03 12:04:45 +0300

    some changes in History and part of Report comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+//import Calendar from "./components/Calendar/Calendar";
 import { History } from "./components/History/History";
+import { Report } from "./components/Report/Report";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import "./app.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <History />
+    
+      <BrowserRouter>
+                <Link to='/report'>
+                  Report
+                </Link>
+                <Link to='/history'>
+                  History
+                </Link>
+              
+              <Routes>
+                <Route path="/report" element={<Report />} />
+                <Route path="/history" element={<History />} />
+              </Routes>
+              </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,53 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState, forwardRef } from "react";
+import "./calendar.scss";
+
+export function Calendar() {
+  const [startDate, setStartDate] = useState(new Date());
+  const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+    <button className="example-custom-input" onClick={onClick} ref={ref}>
+      {value}
+    </button>
+  ));
+
+  function onChangeDateHandler(value) {
+    setStartDate(value);
+  }
+  return (
+    <>
+      <div className="calendar">
+        <DatePicker
+          className="date grey"
+          selected={startDate}
+          onChange={onChangeDateHandler}
+          customInput={<ExampleCustomInput />}
+          showYearPicker
+          withPortal
+          dateFormat="yyyy"
+          yearItemNumber={4}
+        />
+        <DatePicker
+          className="date"
+          selected={startDate}
+          onChange={onChangeDateHandler}
+          customInput={<ExampleCustomInput />}
+          dateFormat="MM/yyyy"
+          showMonthYearPicker
+          withPortal
+        />
+        <DatePicker
+          className="date grey"
+          selected={startDate}
+          onChange={onChangeDateHandler}
+          customInput={<ExampleCustomInput />}
+          dateFormat="dd/MM/yyyy"
+          withPortal
+        />
+      </div>
+      <h3>{`March`}</h3>
+    </>
+  );
+}
+
+export default Calendar;
