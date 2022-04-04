c127f79168dfb3f31f262594624ed976a17db216
Alex Martynov <a.martynov@216base.ru>
2022-04-04 14:21:50 +0300

    Refactoring styles for doughnut

--- src/components/OutputReport/outputReport.tsx
+++ src/components/OutputReport/outputReport.tsx
@@ -4,15 +4,15 @@ import { Doughnut } from 'react-chartjs-2'
 Chart.register(ArcElement)
 /**
  * 
- * @param params Data {id: , categoryId: , amount: , date: }
+ * @param params Where: "income || expences" Data: {id: , categoryId: , amount: , date: }
  * @returns Block with sum
  */
-export function OutputReport(params:any) {
+export function OutputReport(params: any) {
     const arrayHandler = params.param
     let amountOutput = 0
 
     if(arrayHandler !== undefined) {
-        arrayHandler.forEach(function amountHandler(elem :any) {
+        arrayHandler.forEach(function amountHandler(elem: any) {
             return amountOutput = amountOutput + elem.amount
         })
     }
