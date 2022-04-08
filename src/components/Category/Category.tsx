601f1a3f28bd7ef368b1f3bde7f815f5b89e9504
Jenya Poznyak <jendy.light@gmail.com>
2022-04-08 16:42:41 +0300

    icons src fix

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -2,21 +2,13 @@ import React from "react";
 import { Category } from "../../model";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import IconButton from '@mui/material/IconButton';
-import HouseIcon from '@mui/icons-material/House';
-import RestaurantIcon from '@mui/icons-material/Restaurant';
-import SportsTennisIcon from '@mui/icons-material/SportsTennis';
-import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
-import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
-import CheckroomIcon from '@mui/icons-material/Checkroom';
-import MobileFriendlyIcon from '@mui/icons-material/MobileFriendly';
-import AlignHorizontalLeftIcon from '@mui/icons-material/AlignHorizontalLeft';
 import AddIcon from '@mui/icons-material/Add';
 import "./category.scss";
 
 const caregoriesList: Category[] = [
     {
         id: 1,
-        img: "src='../images/Icons/house.png'",
+        img: "../images/Icons/house.png",
         name: "House",
     },
     {
@@ -51,7 +43,7 @@ const caregoriesList: Category[] = [
     },
     {
         id: 8,
-        img: "../images/Icons/all.png",
+        img: "../images/Icons/all_cat.png",
         name: "All Categories",
   },
   ]
@@ -68,53 +60,19 @@ const ExpIcons: React.FC = () => {
                 <AddIcon className='addIcon'/>
             </IconButton>
         </div>
+        
         <div className="cat-block">
-            <div className="cat-item">
-                <div className="cat-icon"><HouseIcon/></div>
-                <p className="cat-name">House</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><RestaurantIcon/></div>
-                <p className="cat-name">Food</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><SportsTennisIcon/></div>
-                <p className="cat-name">Sport</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><HealthAndSafetyIcon/></div>
-                <p className="cat-name">Health</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><DirectionsCarIcon/></div>
-                <p className="cat-name">Car</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><CheckroomIcon/></div>
-                <p className="cat-name">Cloth</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><MobileFriendlyIcon/></div>
-                <p className="cat-name">Mobile</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><AlignHorizontalLeftIcon/></div>
-                <p className="cat-name">All Categories</p>
-            </div>
+            {caregoriesList.map((item) => (
+                
+                <div className="cat-item" key={item.id}>
+                    <div className="cat-icon">
+                        <img className="cat-icon" src={item.img} alt=""/>
+                    </div>
+                <p className="cat-name">{item.name}</p>
+                </div> 
+            ))
+            }
         </div>
-
-            <div className="cat-block">
-                {caregoriesList.map((item) => (
-                    
-                    <div className="cat-item" key={item.id}>
-                        <div className="cat-icon">
-                            <img className="cat-icon" {...item.img} alt=""/>
-                        </div>
-                    <p className="cat-name">{item.name}</p>
-                    </div> 
-                ))
-                }
-            </div>
     </div>
 )}
 
