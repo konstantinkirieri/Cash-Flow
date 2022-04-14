aa8dbe15ef40fcedb91a11972f1d7c55191d0a16
Jenya Poznyak <jendy.light@gmail.com>
2022-04-14 18:57:53 +0300

    AllCategories component added

---
+++ src/components/AllCategories/AllCategories.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Categories from "../Category/Category";
+// import { useNavigate } from "react-router-dom"
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import IconButton from '@mui/material/IconButton';
+// import AddIcon from '@mui/icons-material/Add';
+import "../Category/category.scss";
+
+// const navigate = useNavigate();
+
+const AllCategories: React.FC = () => {
+    return (
+    <div className="category-page">
+        <div className='category-menu'>
+            <IconButton>  
+                <ArrowBackIosIcon className='arrowBack'></ArrowBackIosIcon>
+            </IconButton>    
+            <h2>Categories</h2>
+            <IconButton>
+                <img className="add-icon" src="../images/Icons/add_cat.png" alt="" >
+                    {/* onClick={() => {
+                    navigate("/switch")
+                     }}> */}
+                </img>
+            </IconButton>
+        </div>
+
+        <Categories />
+
+    </div>
+)}
+
+export default AllCategories;
