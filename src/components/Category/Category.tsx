afa96499f6a775e2327fea44e72e4ceca1c44844
Jenya Poznyak <jendy.light@gmail.com>
2022-04-15 11:39:47 +0300

    split in Category

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,122 +1,96 @@
 import React from "react";
 import { Category } from "../../model";
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import IconButton from '@mui/material/IconButton';
-// import HouseIcon from '@mui/icons-material/House';
-// import RestaurantIcon from '@mui/icons-material/Restaurant';
-// import SportsTennisIcon from '@mui/icons-material/SportsTennis';
-// import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
-// import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
-// import CheckroomIcon from '@mui/icons-material/Checkroom';
-// import MobileFriendlyIcon from '@mui/icons-material/MobileFriendly';
-// import AlignHorizontalLeftIcon from '@mui/icons-material/AlignHorizontalLeft';
-import AddIcon from '@mui/icons-material/Add';
+// import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+// import IconButton from '@mui/material/IconButton';
+// import AddIcon from '@mui/icons-material/Add';
 import "./category.scss";
-//import image1 from "../../../images/Icons/house.png";
 
-const caregoriesList: Category[] = [
+export const caregoriesList: Category[] = [
     {
         id: 1,
+        typeId: 2,
         img: "../images/Icons/house.png",
         name: "House",
+        // handleAdd: () => void
     },
     {
         id: 2,
+        typeId: 2,
         img: "../images/Icons/food.png",
         name: "Food",
+        // handleAdd: () => void
     },
     {
         id: 3,
+        typeId: 2,
         img: "../images/Icons/sport.png",
         name: "Sport",
+        // handleAdd: 
     },
     {
         id: 4,
+        typeId: 2,
         img: "../images/Icons/health.png",
         name: "Health",
+        // handleAdd: () => void
     },
     {
         id: 5,
+        typeId: 2,
         img: "../images/Icons/car.png",
         name: "Car",
+        // handleAdd: () => void
     },
     {
         id: 6,
+        typeId: 2,
         img: "../images/Icons/cloth.png",
         name: "Cloth",
+        // handleAdd: 
     },
     {
         id: 7,
+        typeId: 2,
         img: "../images/Icons/lunch.png",
         name: "Lunch",
+        // handleAdd: (e: React.FormEvent) => void
     },
     {
         id: 8,
-        img: "../images/Icons/all_cat.png",
-        name: "All Categories",
-  },
+        typeId: 1,
+        img: "../images/Icons/salary.png",
+        name: "Salary",
+        // handleAdd:
+    },
+    {
+        id: 9,
+        typeId: 1,
+        img: "../images/Icons/other_inc.png",
+        name: "Other income",
+        // handleAdd:
+    },
   ]
 
-export const ExpIcons: React.FC = () => {
+const Categories: React.FC = () => {
   return (
-    <div className="category-page">
-        <div className='category-menu'>
-            <IconButton>  
-                <ArrowBackIosIcon className='arrowBack'></ArrowBackIosIcon>
-            </IconButton>    
-            <h2>Categories</h2>
-            <IconButton>
-                <AddIcon className='addIcon'/>
-            </IconButton>
-        </div>
-        {/* <div className="cat-block">
-            <div className="cat-item">
-                <div className="cat-icon"><HouseIcon/></div>
-                <p className="cat-name">House</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><RestaurantIcon/></div>
-                <p className="cat-name">Food</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><SportsTennisIcon/></div>
-                <p className="cat-name">Sport</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><HealthAndSafetyIcon/></div>
-                <p className="cat-name">Health</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><DirectionsCarIcon/></div>
-                <p className="cat-name">Car</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><CheckroomIcon/></div>
-                <p className="cat-name">Cloth</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><MobileFriendlyIcon/></div>
-                <p className="cat-name">Mobile</p>
-            </div>
-            <div className="cat-item">
-                <div className="cat-icon"><AlignHorizontalLeftIcon/></div>
-                <p className="cat-name">All Categories</p>
-            </div>
-        </div> */}
-
-            <div className="cat-block">
-                {caregoriesList.map((item) => (
-                    
-                    <div className="cat-item" key={item.id}>
-                        <div className="cat-icon">
-                            <img className="cat-icon" src={item.img} alt=""/>
-                        </div>
+      <div className="cat-block">
+            {caregoriesList.map((item) => (
+                
+                <div className="cat-item" key={item.id}>
+                    <div className="cat-icon">
+                        <img className="cat-icon" src={item.img} alt=""/>
+                    </div>
                     <p className="cat-name">{item.name}</p>
-                    </div> 
-                ))
-                }
-            </div>
-    </div>
+                </div> 
+            ))
+            }
+            {/* <div className="cat-item">
+                <div className="cat-icon">
+                    <img className="cat-icon" src="../images/Icons/all_cat.png" alt=""/>
+                </div>
+                <p className="cat-name">All Categories</p>
+            </div> */}
+        </div>
 )}
 
-export default ExpIcons;
+export default Categories;
